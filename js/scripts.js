014b2166e5cbfa4c319178b1f066db02abdd57ee
Vijayant101 <87115118+Vijayant101@users.noreply.github.com>
2021-10-06 12:33:23 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -103,3 +103,7 @@ function topFunction() {
 	document.body.scrollTop = 0; // for Safari
 	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
 }
+
+$(document).bind("contextmenu",function(e){
+	return false;
+	  });

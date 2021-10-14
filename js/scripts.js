05aa1ce45661451c4154820c6ca5878a9783a41b
Vijayant101 <87115118+Vijayant101@users.noreply.github.com>
2021-10-14 12:22:34 +0530

    sw

--- js/scripts.js
+++ js/scripts.js
@@ -107,3 +107,20 @@ function topFunction() {
 (document).bind("contextmenu",function(e){
 	return false;
 	  });
+	
+window.addEventListener('load', e => {
+	new PWAConfApp();
+	registerSW(); 
+	});
+
+async function registerSW() { 
+	if ('serviceWorker' in navigator) { 
+		try {
+			await navigator.serviceWorker.register('sw.js'); 
+		  } catch (e) {
+			alert('ServiceWorker registration failed. Sorry about that.'); 
+		}
+		} else {
+		  document.querySelector('.alert').removeAttribute('hidden'); 
+		}
+	}

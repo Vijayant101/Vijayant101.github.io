c5ceb81d7f1cd88deb87bc29f12698472f9e6cf0
Vijayant101 <87115118+Vijayant101@users.noreply.github.com>
2021-12-02 14:48:06 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -149,7 +149,7 @@ window.addEventListener("appinstalled", evt => {
 
 	window.addEventListener('beforeinstallprompt', (event) => {
   // Prevent the mini-infobar from appearing on mobile.
-  event.preventDefault();
+  // event.preventDefault();
   console.log('👍', 'beforeinstallprompt', event);
   // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
